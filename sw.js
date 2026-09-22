1b1e279438855fec92d5b66f58f2d62f172b4c87
rendeb-dsb <87785469+rendeb-dsb@users.noreply.github.com>
2026-09-22 11:40:30 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mj-pwa-v38';
+const CACHE = 'mj-pwa-v39';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
