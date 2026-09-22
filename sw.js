b7cf6180be1d018b49252e9c45b14fe886a82d9d
rendeb-dsb <87785469+rendeb-dsb@users.noreply.github.com>
2026-09-22 10:32:07 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE = 'mj-v31';
-self.addEventListener('install', event => event.waitUntil(
-  caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.json'])).then(() => self.skipWaiting())
-));
-self.addEventListener('activate', event => event.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
-));
+const CACHE = 'mj-pwa-v35';
+const ASSETS = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
 self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(c => c.put('./', copy));
-      return response;
-    }).catch(() => caches.match('./')));
-  } else {
-    event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
-  }
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
 });
